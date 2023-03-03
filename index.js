52f14a3369bfb1debfecf1b8c9e9884b63d98d47
brico1994 <b.rico.barrilero@gmail.com>
2023-03-03 16:52:43 +0100

    media aritmetica sin probar en index js

--- index.js
+++ index.js
@@ -4,9 +4,16 @@ var cool = require("cool-ascii-faces");
 var app = express();
 var port = process.env.PORT || 12345;
 
+//cool asci faces
 app.get("/faces",(req,res) =>{ //request,response
     res.send(cool());
     console.log("->  New cool face request  <-");
+});
+
+//index brb
+app.get("/samples/BRB",(req,res) =>{ //request,response
+    res.send(mediaGeografica("Almeria", "traveller"));
+    console.log("->  media aritmetica almeria traveller  <-");
 });
 
 app.listen(port,()=>{
