52626f500efaaee6694b24c8020c9f5abf415bee
brico1994 <b.rico.barrilero@gmail.com>
2023-03-20 13:36:49 +0100

    borrar cosas inutiles

--- index.js
+++ index.js
@@ -19,12 +19,12 @@ const BASE_API_URL = "/api/v1";
 app.use("",router); //modularizacion JLN
 
 
-//index brb
+/*//index brb
 app.get("/samples/BRB",(req,res) =>{
     var media = mediaProvincia.mediaGeografica("Almeria","traveller");
     res.send(`la media de viajeros en almeria es: ${media}`);
     console.log("->  media aritmetica almeria traveller  <-");
-});
+});*/
 
 
 
