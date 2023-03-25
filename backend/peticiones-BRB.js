b37917fa278336353bbc4e2d33a461473c953ae5
brico1994 <b.rico.barrilero@gmail.com>
2023-03-25 11:29:41 +0100

    cambio codigo get

--- backend/peticiones-BRB.js
+++ backend/peticiones-BRB.js
@@ -27,7 +27,7 @@ module.exports = (app) => {
                 console.log(`error geting /occupancy: ${err}`);
                 response.sendStatus(500);
             } else {
-                response.status(201).json(dato.map((c) => {
+                response.status(200).json(dato.map((c) => {
                     delete c._id;
                     return c;
                 }));
