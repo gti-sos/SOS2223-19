f5700e5a20a70089191f08465f2f3654d0e6099e
josnavlop4 <joseluisn991@gmail.com>
2023-04-10 14:25:37 +0200

    F08.3

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,13 +2,13 @@
     <body>
         <h1>SOS2223-19</h1>
         <ul>
-        <li><strong>Team</strong><ul>
+        <li><strong>Equipo</strong><ul>
         <li><a href="https://github.com/brico1994">Bruno Alvaro Rico Barrilero</a></li>
         <li><a href="https://github.com/josnavlop4">Jose Luis Navarro Lopez-Montenegro: Estadísticas de ocupación</a></li>
         </ul>
         </li>
-        <li><strong>Project description</strong>: Our source of information are supposed to analize the relation between occupancy in rural tourism and occupancy in camping sites.</li>
-        <li><strong>Repository</strong>: <a href="https://github.com/gti-sos/SOS2223-19">gti-sos/SOS2223-19</a></li>
+        <li><strong>Descripcion del proyecto</strong>: Our source of information are supposed to analize the relation between occupancy in rural tourism and occupancy in camping sites.</li>
+        <li><strong>Repositorio</strong>: <a href="https://github.com/gti-sos/SOS2223-19">gti-sos/SOS2223-19</a></li>
         <li><strong>URL</strong>: <a href="http://sos2223-19.appspot.com/">http://sos2223-19.appspot.com/ </a></li>
         <li><strong>APIs</strong>:<ul>
             <li><a href="/api/v2/occupation-stats">Estadisticas de ocupacion </a>   (creado por <a href="https://github.com/josnavlop4">Jose Luis Navarro López-Montenegro</a>)</li> 
