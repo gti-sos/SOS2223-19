6cff768e9ed32265cd4a9aca9922ef38d28636ad
brico1994 <b.rico.barrilero@gmail.com>
2023-04-14 18:35:08 +0200

    no css

--- frontend/src/routes/Header.svelte
+++ frontend/src/routes/Header.svelte
@@ -2,5 +2,6 @@
     <div class="topnav">
         <a class="active" href="/"> Inicio </a>
         <a href="/occupation-stats"> Estadisticas de ocupacion </a>
+        <a href="/occupancy-of-accomodation-in-rural-tourism"> Estadisticas de acomodacion en turismo rural </a>
     </div>
 </nav>
