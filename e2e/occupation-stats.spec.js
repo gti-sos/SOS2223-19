b0a397ad2df03e90c0d17d0ee10faa943b16c8dd
josnavlop4 <joseluisn991@gmail.com>
2023-05-19 18:38:37 +0200

    A.R 4

---
+++ e2e/occupation-stats.spec.js
@@ -0,0 +1,42 @@
+//@ts-check
+import { test, expect } from'@playwright/test';
+
+test('get occupation stats', async ({ page }) => {
+    await page.goto('https://sos2223-19.appspot.com/');
+  
+    // Click the get started link.
+    await page.getByRole('link', { name: 'Grafo de la estadistica de ocupacion en campings' }).click();
+  
+    // Expects the URL to contain intro.
+    await expect(page).toHaveURL(/.*graphJLN/);
+  });
+
+  test('get analytics', async ({ page }) => {
+    await page.goto('https://sos2223-19.appspot.com/');
+  
+    // Click the get started link.
+    await page.getByRole('link', { name: 'Grafo general' }).click();
+  
+    // Expects the URL to contain intro.
+    await expect(page).toHaveURL(/.*analytics/);
+  });
+
+  test('get uses and integrations', async ({ page }) => {
+    await page.goto('https://sos2223-19.appspot.com/');
+  
+    // Click the get started link.
+    await page.getByRole('link', { name: 'Usos y Integraciones' }).click();
+  
+    // Expects the URL to contain intro.
+    await expect(page).toHaveURL(/.*integrations/);
+  });
+
+  test('get about', async ({ page }) => {
+    await page.goto('https://sos2223-19.appspot.com/');
+  
+    // Click the get started link.
+    await page.getByRole('link', { name: 'Sobre nosotros' }).click();
+  
+    // Expects the URL to contain intro.
+    await expect(page).toHaveURL(/.*about/);
+  });
